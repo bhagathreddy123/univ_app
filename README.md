8d8026683f131b7ee6c499207aab10803d683c75
BhagathReddy-K <bkatta@accela.com>
2019-12-13 23:17:05 +0530

    added meterialize css gem for styling the app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # univ_app
 rails 5.1 ruby 2.5 application
 
+rails _5.1.6_ new univ_app
+
+https://materializecss.com/about.html
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "courses#index"
   get 'about', to: 'pages#about'
+  get 'courses/index', to: 'courses#index'
+  get 'courses/new', to: 'courses#new'
 end
