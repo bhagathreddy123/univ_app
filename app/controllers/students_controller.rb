bfffec15d9f4dd8474547449a5b63ecc18a00162
BhagathReddy-K <bkatta@accela.com>
2019-12-14 23:49:36 +0530

    flash message display and form submition implementation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,5 +6,25 @@ class StudentsController < ApplicationController
   def new
   	@student = Student.new
   end
+
+  def create
+  	@student = Student.new(student_params)
+  	if @student.save
+  		flash[:notice] = "You have successfully signed up"
+  		redirect_to students_path
+  	else
+  		render :new
+  	end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  private
+
+  def student_params
+  	params.require(:student).permit(:name,:email)
+  end
   
 end
